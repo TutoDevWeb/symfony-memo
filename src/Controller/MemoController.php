7b98893d383e882b688167ad949f6a6bcede55d6
TutoDevWeb <TutoDevWeb@outlook.fr>
2022-11-11 10:27:00 +0100

    Une autre façon de faire qui est également proposée par la doc.

--- src/Controller/MemoController.php
+++ src/Controller/MemoController.php
@@ -2,8 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Memo;
-use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\MemoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class MemoController extends AbstractController
 {
     #[Route('/', name: 'app_memo')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(MemoRepository $MemoRepository): Response
     {
 
-        $memos = $doctrine->getRepository(Memo::class)->findAll();
+        $memos = $MemoRepository->findAll();
 
         return $this->render('memo/index.html.twig', ['memos' => $memos]);
     }
